182bad0565bb0a2ab12052ece7070024e331ddb2
19039518 <gurpreetgillfzr00292gmail.com>
2019-11-28 16:45:20 -0500

    lab10 is done

--- app/src/main/java/android/bignerdranch/com/CrimeListActivity.java
+++ app/src/main/java/android/bignerdranch/com/CrimeListActivity.java
@@ -1,12 +1,12 @@
 package android.bignerdranch.com;
 
-
 import androidx.fragment.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity  extends SingleFragmentActivity {
+
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
-}
 
+}
